402cc3c286b342f58e718c19365e4f98cc2cbe16
Afif Ali Saadman <98092569+afifafifafifafifali@users.noreply.github.com>
2025-07-12 20:03:55 +0600

    Update ListPy.py

--- ListPy/ListPy.py
+++ ListPy/ListPy.py
@@ -22,7 +22,7 @@ class ListPy:
         itr = self.head
         llstr = ''
         while itr:
-            llstr += str(itr.data)+' --> ' if itr.next else str(itr.data)
+            llstr += str(itr.data)+',' if itr.next else str(itr.data)
             itr = itr.next
         print(llstr)
 
